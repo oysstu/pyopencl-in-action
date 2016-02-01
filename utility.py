7110d5bd7da99cee592adda832a6422c88d3a8d1
oysstu <oysstu@gmail.com>
2016-02-01 21:14:15 +0100

    Fall back to CPU if no GPU present

--- utility.py
+++ utility.py
@@ -1,18 +1,27 @@
 import pyopencl as cl
 from operator import attrgetter
 
-'''
-Retrieves the GPU device with the most global memory
-'''
-def get_default_device():
+
+def get_default_device() -> cl.Device:
+    '''
+    Retrieves the GPU device with the most global memory if available, otherwise returns the CPU.
+    '''
     platforms = cl.get_platforms()
     gpu_devices = [plat.get_devices(cl.device_type.GPU) for plat in platforms]
     gpu_devices = [dev for devices in gpu_devices for dev in devices]  # Flatten to 1d if multiple GPU devices exists
 
-    if len(gpu_devices) == 0:
-        print('No GPU devices found')
+    if gpu_devices:
+        dev = max(gpu_devices, key=attrgetter('global_mem_size'))
+        print('Using GPU: {}\n'.format(dev.name))
+        return dev
+    else:
+        cpu_devices = [plat.get_devices(cl.device_type.CPU) for plat in platforms]
+        cpu_devices = [dev for devices in cpu_devices for dev in devices]
+        if cpu_devices:
+            dev = max(cpu_devices, key=attrgetter('global_mem_size'))
+            print('Using CPU: {}\n'.format(dev.name))
+            return dev
+        else:
+            print('No suitable OpenCL GPU/CPU devices found')
 
-    dev = max(gpu_devices, key=attrgetter('global_mem_size'))
-    print('Using GPU: ' + dev.name)
-    return dev
 
