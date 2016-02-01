37dcee880c15782172c6a19c493fed1a43105833
oysstu <oysstu@gmail.com>
2016-02-01 14:42:41 +0100

    Print to columns, loop print

--- Ch4 - Kernel programming/float_config.py
+++ Ch4 - Kernel programming/float_config.py
@@ -11,20 +11,21 @@ dev = utility.get_default_device()
 # Check for double floating point features
 fp_flag = dev.single_fp_config
 
-
-print('Floating point features:')
-print('\tDenorm: ' + str(bool(fp_flag & cl.device_fp_config.DENORM)))
-print('\tFused multiply-add: ' + str(bool(fp_flag & cl.device_fp_config.FMA)))
-print('\tINF & NAN: ' + str(bool(fp_flag & cl.device_fp_config.INF_NAN)))
-print('\tRound to INF: ' + str(bool(fp_flag & cl.device_fp_config.ROUND_TO_INF)))
-print('\tRound to nearest: ' + str(bool(fp_flag & cl.device_fp_config.ROUND_TO_NEAREST)))
-print('\tRound to zero: ' + str(bool(fp_flag & cl.device_fp_config.ROUND_TO_ZERO)))
+fp_masks = [('Denorm',              cl.device_fp_config.DENORM),
+            ('Fused multiply-add',  cl.device_fp_config.FMA),
+            ('INF & NAN',           cl.device_fp_config.INF_NAN),
+            ('Round to INF',        cl.device_fp_config.ROUND_TO_INF),
+            ('Round to nearest',    cl.device_fp_config.ROUND_TO_NEAREST),
+            ('Round to zero',       cl.device_fp_config.ROUND_TO_ZERO)]
 
 version_number = float(dev.version.split(' ')[1])
 
 if version_number >= 1.1:
-    print('\tSoft float: ' + str(bool(fp_flag & cl.device_fp_config.SOFT_FLOAT)))
+    fp_masks.append(('Soft float', cl.device_fp_config.SOFT_FLOAT))
 
 if version_number >= 1.2:
-    print('\tCorrectly rounded div sqrt: ' + str(bool(fp_flag & cl.device_fp_config.CORRECTLY_ROUNDED_DIVIDE_SQRT)))
+    fp_masks.append(('Correctly rounded div sqrt', cl.device_fp_config.CORRECTLY_ROUNDED_DIVIDE_SQRT))
+
+print('Floating point features:')
+[print('\t{0:<30}{1:<5}'.format(name, str(bool(fp_flag & mask)))) for name, mask in fp_masks]
 
