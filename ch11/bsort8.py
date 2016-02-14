8cbb0bf4e5a79708c3feba28ffbd39a9213f7b95
oysstu <oysstu@gmail.com>
2016-02-14 11:09:25 +0100

    Ascending/Descending fix

--- ch11/bsort8.py
+++ ch11/bsort8.py
@@ -7,8 +7,7 @@ import numpy as np
 import pyopencl as cl
 import utility
 
-ASCENDING = 0
-DESCENDING = -1
+sort_ascending = True
 
 kernel_src = '''
 #define UP 0
@@ -85,7 +84,7 @@ data_buffer = cl.Buffer(context, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=data)
 local_size = (1,)
 global_size = (1,)
 
-start_event = prog.bsort8(queue, global_size, local_size, data_buffer, np.int32(ASCENDING))
+start_event = prog.bsort8(queue, global_size, local_size, data_buffer, np.int32(0 if sort_ascending else -1))
 
 cl.enqueue_copy(queue, dest=data, src=data_buffer, is_blocking=True)
 
